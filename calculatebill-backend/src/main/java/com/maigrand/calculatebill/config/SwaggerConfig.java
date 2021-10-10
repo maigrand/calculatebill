4a1c10dbaca0e1a5f4d1d0efc609e59bfcb3ffb3
mai <mkmaxz48@gmail.com>
2021-10-10 09:05:43 +0300

    feat: настройка сваггера

--- calculatebill-backend/src/main/java/com/maigrand/calculatebill/config/SwaggerConfig.java
+++ calculatebill-backend/src/main/java/com/maigrand/calculatebill/config/SwaggerConfig.java
@@ -1,8 +1,19 @@
 package com.maigrand.calculatebill.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+import static java.util.Collections.*;
 
 @Configuration
 @EnableSwagger2
@@ -12,5 +23,44 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 })
 public class SwaggerConfig {
 
+    private final Set<String> producesSet = new HashSet<>();
 
+    @Bean
+    public Docket adminApi() {
+        producesSet.add("application/json");
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.maigrand.calculatebill.controller"))
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .build()
+                .produces(producesSet)
+                .useDefaultResponseMessages(false)
+                .directModelSubstitute(Timestamp.class, Long.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .forCodeGeneration(true)
+                .securityContexts(singletonList(securityContext()))
+                .securitySchemes(singletonList(apiKey()))
+                .tags(new Tag("Авторизация", "", 1));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("mykey", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/api/.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return singletonList(
+                new SecurityReference("mykey", authorizationScopes));
+    }
 }
