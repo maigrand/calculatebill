81f8bb7ca882e4214f9242600ce098f019b2315d
mai <mkmaxz48@gmail.com>
2021-10-30 10:24:15 +0300

    feat: Инициализация тестов

---
+++ calculatebill-backend/src/test/java/com/maigrand/calculatebill/TestOrder.java
@@ -0,0 +1,18 @@
+package com.maigrand.calculatebill;
+
+public final class TestOrder {
+
+    public static final int CREATE = 0;
+
+    public static final int CREATE_WITH_EXCEPTION=10;
+
+    public static final int READ=20;
+
+    public static final int UPDATE_WITHOUT_CHANGE=30;
+
+    public static final int UPDATE=40;
+
+    public static final int UPDATE_WITH_EXCEPTION=50;
+
+    public static final int DELETE=60;
+}
