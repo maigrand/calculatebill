41fdf1be6238ef75ad3745ee60e3ab8380318038
Evgeny Gerdt <assmys041298@gmail.com>
2021-11-01 01:36:20 +0300

    Fix

--- calculatebill-frontend/src/components/ui/bills/BillsList.jsx
+++ calculatebill-frontend/src/components/ui/bills/BillsList.jsx
@@ -4,27 +4,27 @@ import BillItem from "./BillItem";
 import "../../../styles/ui/_billsListComp.scss";
 
 const BillsList = (props) => {
-    return (
+  return (
       <div className="bills-list__container">
-          <div className="bills-list__wrapper">
-              <header>
-                  { props.amount && (<strong>Bills amount: {props.amount}</strong>) }
-              </header>
+        <div className="bills-list__wrapper">
+          <header>
+            {props.amount && (<strong>Bills amount: {props.amount}</strong>)}
+          </header>
 
-              <main>
-                  { props.bills.map((bill, index) =>
-                      <BillItem
-                          key={index}
-                          guests={bill.guests.length}
-                          name={bill.name}
-                          date={bill.date}
-                          id={bill.id}
-                      />)
-                  }
-              </main>
-          </div>
+          <main>
+            {props.bills?.map((bill, index) =>
+                <BillItem
+                    key={index}
+                    guests={bill.guests.length}
+                    name={bill.name}
+                    date={bill.date}
+                    id={bill.id}
+                />)
+            }
+          </main>
+        </div>
       </div>
-    );
+  );
 };
 
 export default BillsList;
